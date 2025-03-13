58388862db46942d5a9f9e238abcc18784476494
Jawad <Jawadsadiq7373@gmail.com>
2025-03-13 12:37:09 -0700

    the static part of this small  project

---
+++ trip-pack/src/App.js
@@ -0,0 +1,54 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😎 trip?</h3>
+      <select>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        <Item />
+      </ul>
+    </div>
+  );
+}
+
+function Item() {
+  return (
+    <li>
+      <span>12 Socks</span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items in your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
